ba78e2f4711b7bc4989f81f3f90369aebc327492
Sednek <sednek2@yandex.ru>
2023-04-03 15:09:12 +0300

    wrong hw

---
+++ src/homework/Bicycle.java
@@ -0,0 +1,11 @@
+package homework;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
