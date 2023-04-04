0c82351bd2871139a10c135809d19ce474b57c0c
Sednek <sednek2@yandex.ru>
2023-04-04 16:17:07 +0300

    fix

--- src/homework/ServiceStation.java
+++ src/homework/ServiceStation.java
@@ -2,17 +2,23 @@ package homework;
 
 public class ServiceStation {
     public void check(Car car) {
+
         car.checkTyres();
+
         car.checkEngine();
     }
 
     public void check(Truck truck) {
+
         truck.checkTyres();
+
         truck.checkEngine();
+
         truck.checkTrailer();
     }
 
     public void check(Bicycle bicycle) {
+
         bicycle.checkTyres();
     }
 }
